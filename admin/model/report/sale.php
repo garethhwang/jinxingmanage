96e9de87b09e2393e8a650a5bd9d8fc5371a834e
renxiaopeng <531920754@sina.com>
2017-02-27 16:37:57 +0800

    customer web update

--- admin/model/report/sale.php
+++ admin/model/report/sale.php
@@ -25,6 +25,8 @@ class ModelReportSale extends Model {
 			$implode[] = "'" . (int)$order_status_id . "'";
 		}
 
+		var_dump($this->config->get('config_complete_status'));
+
 		$order_data = array();
 
 		for ($i = 0; $i < 24; $i++) {
