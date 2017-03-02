c496ab9985cb922be261422ffd3e035794fabe54
renxiaopeng <531920754@sina.com>
2017-03-02 12:00:18 +0800

    chart learn log

---
+++ admin/language/en-gb/extension/dashboard/processed_order.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renxiaopeng
+ * Date: 2017/3/1
+ * Time: 21:42
+ */
+// Heading
+$_['heading_title']    = 'Paid Total Orders';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified dashboard orders!';
+$_['text_edit']        = 'Edit Dashboard Orders';
+$_['text_view']        = 'View more...';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_width']      = 'Width';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify dashboard orders!';
---
+++ admin/language/zh-cn/extension/dashboard/processed_order.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renxiaopeng
+ * Date: 2017/3/1
+ * Time: 21:44
+ */
+
+
+// Heading
+$_['heading_title'] = '付款总订单数量';
+
+// Text
+$_['text_view']     = '查看更多......';
