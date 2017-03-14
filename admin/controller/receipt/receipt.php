9aa4cb93eb9733b6792aebb1f4cbe71815a219ec
renxiaopeng <531920754@sina.com>
2017-03-14 17:32:57 +0800

    chart learn log

--- admin/controller/receipt/receipt.php
+++ admin/controller/receipt/receipt.php
@@ -235,6 +235,67 @@ class ControllerReceiptReceipt extends Controller
     }
 
     public function visit_delete(){
+        $this->load->language('receipt/receipt');
+
+        $this->document->setTitle($this->language->get('heading_title'));
+
+        $this->load->model('receipt/receipt');
+
+        if (isset($this->request->post['selected'])) {
+            var_dump($this->request->post['selected']);
+            foreach ($this->request->post['selected'] as $receipt_history_id) {
+                //$this->model_customer_customer->deleteCustomer($receipt_history_id);
+            }
+/*
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            $url = '';
+
+            if (isset($this->request->get['filter_name'])) {
+                $url .= '&filter_name=' . urlencode(html_entity_decode($this->request->get['filter_name'], ENT_QUOTES, 'UTF-8'));
+            }
+
+            if (isset($this->request->get['filter_email'])) {
+                $url .= '&filter_email=' . urlencode(html_entity_decode($this->request->get['filter_email'], ENT_QUOTES, 'UTF-8'));
+            }
+
+            if (isset($this->request->get['filter_customer_group_id'])) {
+                $url .= '&filter_customer_group_id=' . $this->request->get['filter_customer_group_id'];
+            }
+
+            if (isset($this->request->get['filter_status'])) {
+                $url .= '&filter_status=' . $this->request->get['filter_status'];
+            }
+
+            if (isset($this->request->get['filter_approved'])) {
+                $url .= '&filter_approved=' . $this->request->get['filter_approved'];
+            }
+
+            if (isset($this->request->get['filter_ip'])) {
+                $url .= '&filter_ip=' . $this->request->get['filter_ip'];
+            }
+
+            if (isset($this->request->get['filter_date_added'])) {
+                $url .= '&filter_date_added=' . $this->request->get['filter_date_added'];
+            }
+
+            if (isset($this->request->get['sort'])) {
+                $url .= '&sort=' . $this->request->get['sort'];
+            }
+
+            if (isset($this->request->get['order'])) {
+                $url .= '&order=' . $this->request->get['order'];
+            }
+
+            if (isset($this->request->get['page'])) {
+                $url .= '&page=' . $this->request->get['page'];
+            }
+
+            $this->response->redirect($this->url->link('customer/customer', 'token=' . $this->session->data['token'] . $url, true));
+*/
+        }
+
+        $this->getList();
 
     }
 
