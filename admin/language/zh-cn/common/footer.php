fdd201c8f9ae78e74e777abf3c7c7abce1d9eaae
renxiaopeng <531920754@sina.com>
2017-04-11 12:11:11 +0800

    chart learn log

--- admin/language/zh-cn/common/footer.php
+++ admin/language/zh-cn/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] 	= '<a href="http://www.mycncart.com">MyCnCart</a> &copy; 2015-' . date('Y') . ' 版权所有。';
+$_['text_footer'] 	= '<a href="http://manage.jinxingjk.com">金杏管理系统</a> &copy; 2015-' . date('Y') . ' 版权所有。';
 $_['text_version'] 	= '版本号 %s';
--- admin/language/zh-cn/common/header.php
+++ admin/language/zh-cn/common/header.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']        = 'MyCnCart';
+$_['heading_title']        = '金杏管理系统';
 
 // Text
 $_['text_order']           = '订单';
@@ -17,7 +17,7 @@ $_['text_affiliate']       = '加盟';
 $_['text_store']           = '商店';
 $_['text_front']           = '商店前台';
 $_['text_help']            = '帮助';
-$_['text_homepage']        = 'MyCnCart官网';
+$_['text_homepage']        = '金杏管理系统';
 $_['text_support']         = '支持论坛';
 $_['text_documentation']   = '手册教程';
 $_['text_logout']          = '安全退出';
